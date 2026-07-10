484a8dcb6867e71ff9284e183978e0042041a3b0
Vandana Subramanian <vandana2606v@gmail.com>
2026-07-10 08:04:19 -0400

    Initialize React frontend with Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>CampusRank</h1>
+
+      <h2>Battle of the Bands</h2>
+
+      <p>
+        Welcome to CampusRank!
+      </p>
+
+      <button>Create Event</button>
+    </div>
+  );
+}
+
+export default App;
